c1da6b90e21d1570505cb2a4d21dd22be0495032
佐藤裕 <it@hirpssyi.net>
2017-08-30 10:14:50 +0900

    Add Setting display and authenticaition

---
+++ Apo-Chan_Xamarin_Forms/Apo_Chan/ViewModels/SettingViewModel.cs
@@ -0,0 +1,82 @@
+﻿using Apo_Chan.Items;
+using Apo_Chan.Managers;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
+using System;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace Apo_Chan.ViewModels
+{
+    public class SettingViewModel : BaseViewModel
+    {
+        #region Variable and Property
+        public UserItem User { get; set; }
+
+        private ObservableCollection<SettingMenuVMItem> settingItems;
+        public ObservableCollection<SettingMenuVMItem> SettingItems
+        {
+            get
+            {
+                return settingItems;
+            }
+            set
+            {
+                SetProperty(ref this.settingItems, value);
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+        //constructor
+        public SettingViewModel(INavigationService navigationService, IPageDialogService dialogService)
+            : base(navigationService, dialogService)
+        {
+            this.User = UserItem.GetCachedUserItem();
+
+            SettingItems = new ObservableCollection<SettingMenuVMItem>();
+            SettingItems.Add(new SettingMenuVMItem("SignOut", "SignOut From APO-Chan.", navigateSignOut));
+        }
+        #endregion
+
+        #region Function
+        private async void navigateSignOut()
+        {
+            this.IsBusy = true;
+            if (App.Authenticator != null)
+            {
+                await this.navigationService.NavigateAsync("/NavigationPage/SignIn");
+            }
+            //// Set syncItems to true to synchronize the data on startup when offline is enabled.
+            //if (authenticated == true)
+            //{
+            //    await navigationService.NavigateAsync("SignIn");
+            //}
+            this.IsBusy = false;
+        }
+
+        #endregion
+
+        #region InnerClass
+        public class SettingMenuVMItem
+        {
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public DelegateCommand ItemTappedCommand { get; private set; }
+
+            public SettingMenuVMItem(string title, string description, Action executeMethod)
+            {
+                this.Title = title;
+                this.Description = description;
+                this.ItemTappedCommand = new DelegateCommand(executeMethod);
+            }
+
+
+        }
+        #endregion
+    }
+}
---
+++ Apo-Chan_Xamarin_Forms/Apo_Chan/Views/Setting.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Apo_Chan.Items;
+
+namespace Apo_Chan.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Setting : ContentPage
+    {
+        public Setting()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
