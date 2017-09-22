181ea95ab50e6d32e62d1acebd1128e374708270
佐藤裕 <it@hirpssyi.net>
2017-09-22 16:14:14 +0700

    forget Commit.

---
+++ Apo-Chan_Xamarin_Forms/Apo_Chan/Models/Interface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apo_Chan.Models
+{
+    /// <summary>
+    /// Authentication Interface
+    /// </summary>
+    public interface IAuthenticate
+    {
+        Task<bool> AuthenticateAsync(Constants.EProviderType providerType);
+        Task<bool> SignOutAsync();
+    }
+
+}
