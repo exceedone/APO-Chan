9278bb92cd95e5ca80b1663f90577bb5e4942346
佐藤裕 <it@hirossyi.net>
2017-09-29 09:59:27 +0700

    Why Ignore new class!!??

---
+++ Apo-Chan_Xamarin_Forms/Apo_Chan/Models/Utils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apo_Chan.Models
+{
+    class Utils
+    {
+    }
+
+    public static class UnixTime
+    {
+        private static readonly DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        /*===========================================================================*/
+        /**
+         * 現在時刻からUnixTimeを計算する.
+         *
+         * @return UnixTime.
+         */
+        public static long Now()
+        {
+            return (FromDateTime(DateTime.UtcNow));
+        }
+
+        /*===========================================================================*/
+        /**
+         * UnixTimeからDateTimeに変換.
+         *
+         * @param [in] unixTime 変換したいUnixTime.
+         * @return 引数時間のDateTime.
+         */
+        public static DateTime FromUnixTime(long unixTime)
+        {
+            return UNIX_EPOCH.AddSeconds(unixTime).ToLocalTime();
+        }
+
+        /*===========================================================================*/
+        /**
+         * 指定時間をUnixTimeに変換する.
+         *
+         * @param [in] dateTime DateTimeオブジェクト.
+         * @return UnixTime.
+         */
+        public static long FromDateTime(DateTime dateTime)
+        {
+            double nowTicks = (dateTime.ToUniversalTime() - UNIX_EPOCH).TotalSeconds;
+            return (long)nowTicks;
+        }
+    }
+}
