821670d1bef9d94b491d8821f2f716cd7fda2136
Long Nguyen <longnt@trithucmoi.co>
2017-10-17 16:29:12 +0700

    Fix Launch Screen for iOS.

---
+++ Apo-Chan_Xamarin_Forms/iOS/AppDelegate.designer.cs
@@ -0,0 +1,20 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace Apo_Chan.iOS
+{
+    [Register ("AppDelegate")]
+    partial class AppDelegate
+    {
+        void ReleaseDesignerOutlets ()
+        {
+        }
+    }
+}
